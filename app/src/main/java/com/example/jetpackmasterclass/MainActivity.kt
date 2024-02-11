d82da9fe6a07319ccbd6e20acbbdb3519b56dd7c
Nikolay Irtuganov <vongu3@gmail.com>
2024-02-11 22:58:30 +0300

    create weight picker

--- app/src/main/java/com/example/jetpackmasterclass/MainActivity.kt
+++ app/src/main/java/com/example/jetpackmasterclass/MainActivity.kt
@@ -3,21 +3,18 @@ package com.example.jetpackmasterclass
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackmasterclass.ui.theme.JetpackMasterclassTheme
@@ -26,85 +23,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyCanvas()
-//            JetpackMasterclassTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
+            var weight by remember {
+                mutableStateOf(80)
+            }
+            Box(modifier = Modifier.fillMaxSize()) {
+                Scale(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp)
+                        .align(Alignment.BottomCenter),
+                    style = ScaleStyle(scaleWidth = 150.dp)
+                ) {
+                    weight = it
+                }
+            }
         }
-    }
-}
-
-@Composable
-fun MyCanvas() {
-    Canvas(
-        modifier = Modifier
-            .padding(20.dp)
-            .size(300.dp)
-    ) {
-        drawRect(
-            color = Color.Black,
-            size = size
-        )
-        drawRect(
-            color = Color.Red,
-            topLeft = Offset(100f, 100f),
-            size = Size(100f, 100f),
-            style = Stroke(
-                width = 3.dp.toPx(),
-
-            )
-        )
-        drawCircle(
-            brush = Brush.radialGradient(
-                colors = listOf(Color.Red, Color.Yellow),
-                center = center,
-                radius = 100f
-            ),
-            radius = 100f
-        )
-        drawArc(
-            color = Color.Green,
-            startAngle = 0f,
-            sweepAngle = 270f,
-            useCenter = false,
-            topLeft = Offset(100f, 500f),
-            size = Size(200f, 200f),
-            style = Stroke(
-                width = 3.dp.toPx()
-            )
-        )
-        drawOval(
-            color = Color.Magenta,
-            topLeft = Offset(500f, 100f),
-            size = Size(200f, 300f)
-        )
-        drawLine(
-            color = Color.Cyan,
-            start = Offset(300f, 700f),
-            end = Offset(700f, 700f),
-            strokeWidth = 5.dp.toPx()
-        )
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackMasterclassTheme {
-        Greeting("Android")
     }
 }
